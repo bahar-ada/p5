0452493afdf68093a5ac03069e2991f324d0e304
bahar <bahar.adamiat@gmail.com>
2024-12-08 08:55:57 +0330

    first commit

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(800, 800);
+  background(0);
+  //ellipse(300,300,150,150)
+  //rect(50,50,200,200)
+  // fill(100, 100, 250);
+   //stroke(100, 100, 250);
+  // triangle(250, 50, 90, 300, 300, 400)
+  //line(0,0,300,300);
+
+  //fill(255);
+  //strokeWeight(20);
+  //point(400,400);
+
+  // stroke(0);
+  // strokeWeight(5);
+  // quad(50, 20, 400, 100, 450, 450, 20, 470)
+}
+
+function draw() {
+  //background(0);
+  //fill(255);
+
+  //rect(mouseX, mouseY, 50,50)
+  stroke(0,255,0);
+  //strokeWeight(5)
+  //point(mouseX, mouseY);
+  line(400, 400, mouseX, mouseY);
+
+}
